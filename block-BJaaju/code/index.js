345f1957c79b300c4f71b54e13c4d8f5d248eadd
Kushal Nandwani <82793018+knandwani98@users.noreply.github.com>
2022-10-03 23:23:51 +0530

    done

--- block-BJaaju/code/index.js
+++ block-BJaaju/code/index.js
@@ -1,35 +1,50 @@
 const input = document.querySelector('input');
 const root = document.querySelector('.root');
 
+let url =
+  'https://api.unsplash.com/photos/?client_id=fN2RrT5BFsErXgXjqpsbGw0hPGgfMhqEI2BtI0dEOqg';
+
+let searchURL = (query) => {
+  return (
+    'https://api.unsplash.com/search/photos/?&query=' +
+    query +
+    '&client_id=fN2RrT5BFsErXgXjqpsbGw0hPGgfMhqEI2BtI0dEOqg'
+  );
+};
+
 function createUI(info) {
-  console.log(info.results);
-  info.results.forEach((elm) => {
-    let img = document.createElement(img);
-    img.src = elm.urls.small;
-    root.append(img);
+  info.forEach((elm) => {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    img.src = elm.urls.thumb;
+    li.append(img);
+    root.append(li);
   });
 }
 
-input.addEventListener('keyup', (event) => {
-  let url =
-    'https://api.unsplash.com/search/photos?&query=' +
-    input.value +
-    '&client_id=fN2RrT5BFsErXgXjqpsbGw0hPGgfMhqEI2BtI0dEOqg';
+function fetch(url, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
 
+  xhr.onload = () => {
+    callback(JSON.parse(xhr.response));
+  };
+
+  xhr.send();
+}
+
+input.addEventListener('keyup', (event) => {
   if (event.keyCode === 13 && input.value) {
     root.innerHTML = '';
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-
-    xhr.onload = function () {
-      createUI(JSON.parse(xhr.response));
-    };
-
-    xhr.send();
+    fetch(searchURL(input.value), (images) => {
+      createUI(images.results);
+    });
 
     input.value = '';
   }
 });
+
+fetch(url, createUI);
 
 // fN2RrT5BFsErXgXjqpsbGw0hPGgfMhqEI2BtI0dEOqg
